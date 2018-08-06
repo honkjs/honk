e35a84842bc4ecd1db7f087731ea5ac9ea651fc0
Kellen Piffner <k@8labs.com>
2018-08-06 09:58:34 -0700

    More api docs.  Tweaking components interfaces.  Updating deps.

--- injector/src/index.ts
+++ injector/src/index.ts
@@ -2,10 +2,21 @@ import { IHonkMiddlewareCreator } from '@honkjs/honk';
 
 declare module '@honkjs/honk' {
   interface IHonk {
+    /**
+     * Runs the specified action, injecting any honk services into it,
+     * and returning the results.
+     *
+     * @template T The return value type.  May be void.
+     * @param {(services: any) => T} action A function that accepts services.
+     * @returns {T} Any results from the action function.
+     */
     <T>(action: (services: any) => T): T;
   }
 }
 
+/**
+ * Creates a middleware to add injector to honk.
+ */
 export default function createMiddleware(): IHonkMiddlewareCreator {
   return (app, next) => (args) => {
     // one argument of type function = call it, passing in services
